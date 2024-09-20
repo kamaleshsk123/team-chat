96c44096911fe9747df7d7a0b19ca8aaa765d691
Kamalesh <shanmugakamalesh.mhash8.io>
2024-09-20 19:11:43 +0530

    [Added]: Manage Members, Channel Creation, Delete & Leave server modal, Search Server Modal

--- components/providers/modal-provider.tsx
+++ components/providers/modal-provider.tsx
@@ -4,6 +4,10 @@ import { CreateServerModal } from "@/components/modals/create-server-modal";
 import { useModal } from "@/hooks/use-modal-store";
 import { InviteModal } from "@/components/modals/invite-modal";
 import { EditServerModal } from "@/components/modals/edit-server-modal";
+import { MembersModal } from "@/components/modals/members-modal";
+import { CreateChannelModal } from "@/components/modals/create-channel-modal";
+import { LeaveServerModal } from "@/components/modals/leave-server-modal";
+import { DeleteServerModal } from "@/components/modals/delete-server-modal";
 
 export const ModalProvider = () => {
   const [isMounted, setIsMounted] = useState(false);
@@ -23,6 +27,10 @@ export const ModalProvider = () => {
       <CreateServerModal />
       <InviteModal />
       <EditServerModal />
+      <MembersModal />
+      <CreateChannelModal />
+      <LeaveServerModal />
+      <DeleteServerModal />
     </>
   );
 };
